0e2d84d529e5031281e5ff13d9ea2414bf05729d
Brandon DeVault <brandondevault@gmail.com>
2019-04-05 09:00:24 -0500

    Update Ping-Sweep.ps1

--- Ping-Sweep.ps1
+++ Ping-Sweep.ps1
@@ -1,4 +1,4 @@
 # Basic Ping-Sweep
 1..100 | ForEach-Object {
-    Write-Output "192.168.86.$_"; ping -n 1 -w 100 192.168.86.$_
+    Write-Output "192.168.1.$_"; ping -n 1 -w 100 192.168.1.$_
 } | Select-String ttl
